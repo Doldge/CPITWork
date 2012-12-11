04c2bc995b6852675f1ea50edc5e94d0b009a154
Doldge <cdt60@hotmail.com>
2012-12-11 16:30:38 +1300

    Update pythonPayRoll.py

--- pythonPayRoll.py
+++ pythonPayRoll.py
@@ -2,89 +2,145 @@
     written by Callum Trayner. '''
 from decimal import Decimal
 
+
+def initialise():
+    ''' This function inialises the variables that the system
+    is going to use, it then calls the onRun function.'''
+    global dir, TWOPLACES, outputFile, Line2, Line4, Line15, Line17, Line19
+    global pageCount, lineCount, employeeCount, totals
+    print("Starting")
+    dir = "."
+    TWOPLACES = Decimal(10) ** -2
+    outputFile = ""
+    Line2 = ("WC_LINE2\n          A.B. CREEDEE CO. LTD." +
+             "    PPAY PERIOD 01 PAGE ")
+    Line4 = ("WC_LINE4\nNUMBER NAME                 GROSS   TAX" +
+             "     UNION   MORT    NETT\n\n")
+    Line15 = ("WD_LINE15\nNUMBER OF EMPLOYEES      ")
+    Line17 = "WD_LINE17\n                                 TOTAL TAX   "
+    Line19 = "WD_LINE19\n                                 TOTAL NETT  "
+    pageCount = 0
+    lineCount = 16
+    employeeCount = 0
+    totals = {'gross': 0, 'tax': 0, 'nett': 0, 'union': 0, 'mortgage':
+              0, 'employees': 0, 'exceptions': 0}
+    onRun()
+    print("stopping")
+
+
 def onRun():
-    inputFile = open("C:\Games\CPIT\Y3 S1\PR301\Assignment2\PAY4DATA.txt",
-                     'r')
+    '''The onRun function reads the inputfile, and collects each
+    individual employees data from the file, it then calls the
+    calc employee data function and the set totals function.'''
+    print("Initialising")
+    global pageCount, employeeCount, dir
+    inputFile = open(dir + '\PAY4DATA.txt', 'r')
     inputFileArray = inputFile.readlines()
     inputFile.close()
-    global pageCount
-    pageCount = 1
-    employeeCount = 1
-    printPageHeading()
-    while employeeCount < len(inputFileArray):
-        employeeData = {'number' : inputFileArray[employeeCount][:6],
-                        'name' : inputFileArray[employeeCount][6:26],
-                        'hours' : Decimal(inputFileArray[employeeCount]
-                                          [26:30]).quantize(TWOPLACES),
-                        'wageRate' : Decimal(inputFileArray[employeeCount]
-                                             [30:34]).quantize(TWOPLACES),
-                        'unionFee' : Decimal(inputFileArray[employeeCount]
-                                             [34:38]).quantize(TWOPLACES),
-                        'mortgage' : Decimal(inputFileArray[employeeCount]
-                                             [38:43]).quantize(TWOPLACES)}
+    pageCount = 0
+    inputCount = 0
+    while inputCount < len(inputFileArray):
+        '''read the data, including the decimal place!
+        n.b the inputs should contain the decimal place, or a space where
+        needs to be.'''
+        employeeData = {'number': inputFileArray[inputCount][:6],
+                        'name': inputFileArray[inputCount][6:26],
+                        'hours': Decimal(inputFileArray[inputCount]
+                                          [26:31]).quantize(TWOPLACES),
+                        'wageRate': Decimal(inputFileArray[inputCount]
+                                             [31:36]).quantize(TWOPLACES),
+                        'unionFee': Decimal(inputFileArray[inputCount]
+                                             [36:41]).quantize(TWOPLACES),
+                        'mortgage': Decimal(inputFileArray[inputCount]
+                                             [41:47]).quantize(TWOPLACES)}
+        print("just read")
+        for i in employeeData:
+            print(str(employeeData[i]))
         employeeData = calcEmployeeValues(employeeData)
         setTotals(employeeData, employeeCount)
-        employeeCount +=1
+        inputCount +=1
     onFinnish()
 
+
 def printPageHeading():
-    global Line2, Line4, outputFile
-    Line2 = Line2 + str(pageCount) +' \n\n'
-    outputFile = Line2 + Line4
+    '''This function adds the page headings to the
+    output file.'''
+    global Line4
+    print("print Headings")
+    printFirstLine()
+    setOutput(Line4)
+
 
 def setOutput(line):
-    global outputFile, pageCount
-    lineCount = 0
-    for i in outputFile:
-        if i == '\n':
-            lineCount += 1
-    if lineCount > 13:
-        # 13, because I've added an empty line at the top of the page
-        #start a new page, what should it look like? do I just add a couple
-        #empty lines?
-        pageCount = pageCount +1
+    '''This function takes a line input, and adds it to
+    the output file.'''
+    global outputFile
+    print("just putting out " + str(line))
     outputFile = outputFile + line
 
+
 def calcEmployeeValues(employee):
+    '''This function takes an employee which contains employee data
+    from the input file, and then calculates the rest of the employees
+    data, such as their net and gross earnings. If there nett earnings
+    are not enough to pay their mortgage or union fee, then there fees
+    are set to stars (*****).'''
+    global lineCount, employeeCount
+    print("Dealing with Employee ")
+    for i in employee:
+        print(str(employee[i]))
+    employeeCount +=1
+    print("Calc Values")
     employee['grossEarnings'] = employee['hours'] * employee['wageRate']
-    employee['grossEarnings'] = Decimal(employee['grossEarnings']
-                                        ).quantize(TWOPLACES)
-    employee['tax'] = employee['grossEarnings'] * Decimal(0.25)
+    employee['grossEarnings'] = (Decimal(employee['grossEarnings'])
+                                 .quantize(TWOPLACES))
+    employee['tax'] = employee['grossEarnings'] * Decimal('0.25')
     employee['tax'] = Decimal(employee['tax']).quantize(TWOPLACES)
-    employee['super'] = employee['grossEarnings'] * Decimal(0.06)
+    employee['super'] = employee['grossEarnings'] * Decimal('0.06')
     employee['super'] = Decimal(employee['super']).quantize(TWOPLACES)
     employee['tempDeductable'] = employee['tax'] + employee['super']
     employee['nettEarnings'] = (employee['grossEarnings'] -
                                 employee['tempDeductable'])
-    employee['nettEarnings'] =Decimal(employee['nettEarnings']
-                                      ).quantize(TWOPLACES)
+    employee['nettEarnings'] =(Decimal(employee['nettEarnings'])
+                               .quantize(TWOPLACES))
     if employee['unionFee'] <= employee['nettEarnings']:
         employee['nettEarnings'] = (employee['nettEarnings'] -
                                     employee['unionFee'])
     else:
         employee['unionFee'] = "  *****"
     if employee['mortgage'] <= employee['nettEarnings']:
-         employee['nettEarnings'] = (employee['nettEarnings'] -
+        employee['nettEarnings'] = (employee['nettEarnings'] -
                                      employee['mortgage'])
     else:
         employee['mortgage'] = "  *****"
-    employee['grossEarnings'] = formatStr(employee['grossEarnings'], 6)
-    employee['tax'] = formatStr(employee['tax'], 6)
-    employee['unionFee'] = formatStr(employee['unionFee'], 6)
-    employee['mortgage'] = formatStr(employee['mortgage'], 6)
-    employee['nettEarnings'] = formatStr(employee['nettEarnings'], 6)
-
+    print("Gross: " + str(employee["grossEarnings"]))
+    print("Set up Line: " + employee['name'])
+    employee['grossEarnings'] = formatStr(employee['grossEarnings'], 7)
+    employee['tax'] = formatStr(employee['tax'], 7)
+    employee['unionFee'] = formatStr(employee['unionFee'], 7)
+    employee['mortgage'] = formatStr(employee['mortgage'], 7)
+    employee['nettEarnings'] = formatStr(employee['nettEarnings'], 7)
     employeeLine = (employee['number'] + ' ' + employee['name'] + ' '
                     + employee['grossEarnings'] + ' ' + employee['tax'] +
                     ' ' + employee['unionFee'] + ' ' +
                     employee['mortgage'] + ' ' + employee['nettEarnings']
                     + '\n')
+    for i in employeeLine:
+        if i == '\n':
+            lineCount = lineCount + 1
+    if lineCount > 12:
+        lineCount = 0
+        printPageHeading()
     setOutput(employeeLine)
     return employee
 
+
 def setTotals(employee, employeeCount):
+    '''This function takes an employee data set and adds it to
+    the group total, it then increments employee count.'''
     error = False
     global totals
+    print("Accumulating Totals")
     try:
         union = totals['union'] + Decimal(employee['unionFee'])
     except:
@@ -97,23 +153,30 @@ def setTotals(employee, employeeCount):
         error = True
     if error == True:
         totals['exceptions'] = totals['exceptions'] + 1
-    totals['gross'] = totals['gross'] + Decimal(employee['grossEarnings'])
+    #COBOL App thinks gross = sum(nett), and nett = sum(gross)
+    totals['gross'] = totals['gross'] + Decimal(employee['nettEarnings'])
     totals['tax'] = totals['tax'] + Decimal(employee['tax'])
-    totals['nett'] = totals['nett'] + Decimal(employee['nettEarnings'])
+    totals['nett'] = totals['nett'] + Decimal(employee['grossEarnings'])
     totals['union'] = union
     totals['mortgage'] = mortgage
     totals['employees'] = employeeCount
 
+
 def onFinnish():
-    global Line15, Line17, Line19, totals, outputFile
+    '''This function adds the final group totals to the output
+    file, and then prints/saves the output file. After which, it
+    closes the output file from memory.'''
+    global Line15, Line17, Line19, totals, outputFile, pageCount
+    print("Tidying up")
     totals['gross'] = formatStr(totals['gross'], 8, True)
     totals['tax'] = formatStr(totals['tax'], 8, True)
     totals['nett'] = formatStr(totals['nett'], 8, True)
     totals['union'] = formatStr(totals['union'], 8, True)
     totals['mortgage'] = formatStr(totals['mortgage'], 8, True)
-    totals['employees'] = formatStr(totals['employees'], 2)
-    totals['exceptions'] = formatStr(totals['exceptions'], 2)
-
+    totals['employees'] = formatStr(totals['employees'], 3)
+    totals['exceptions'] = formatStr(totals['exceptions'], 3)
+    print("Print Totals")
+    printFirstLine()
     Line15 = (Line15 + totals['employees'] + '     TOTAL GROSS ' +
               totals['gross'] + '   TOTAL UNION '
               + totals['union'] + '\n\n')
@@ -124,57 +187,95 @@ def onFinnish():
     Line19 = (Line19 + totals['nett'] + '   EXCEPTIONS  ' +
               totals['exceptions'])
     setOutput(Line19)
-    print(outputFile)
-    outFile = open("C:\Games\CPIT\Y3 S1\PR301\Assignment2\PAY4OUTPUT.txt",
+    #print(outputFile)
+    outFile = open(dir + "\PAY4OUTPUT.txt",
                    "w")
     outFile.write(outputFile)
     outFile.close()
 
+
+def printFirstLine():
+    '''this function resets the line feed to
+    position 0,0 and prints the first line.'''
+    global outputFile, Line2, pageCount
+    print("print First line")
+    outputFile = outputFile + '\f'
+    pageCount = pageCount +1
+    line = Line2 + str(pageCount) +' \n\n'
+    setOutput(line)
+
+
 def formatStr(input, length, isComma=False):
-    '''this function is mostly magic. the +1 is to include the 
+    '''this function is mostly magic. the +1 is to include the
     final (inclusive) character in the string,
     otherwise it'll stop at inputSize (non inclusive).
-    the length - 1 is to account for the i +1'''
+    the length - 1 is to account for the i +1
+    all of the funny dotPos stuff is to only collect
+    the first x-set numbers, and not the last x-set
+    of numbers (re:Truncating correctly)'''
     spaces =''
+    dotPos = length -3
     spaceCount = 0
+    truncDifference = dotPos
     output = str(input)
-    for inputSize, c in enumerate(output):
-        pass
+    for inputSize, inputChar in enumerate(output):
+        try:
+            int(inputChar)
+        except ValueError:
+            if inputChar != '*':
+                '''if inputchar is a '.' and not a '*'
+                then set dot-position to the current position.
+                the +1 is for including the dot'''
+                dotPos = inputSize+1
+    inputSize = inputSize +1
     if isComma == True:
-        if inputSize >= (length-1):
-            length = length +1
-            output = output[:2] + ',' + output[2:inputSize+2]
-            '''+2 for inclusiveness and comma.
-            length = max size of the string, including comma.
+        if ((inputSize) >= (length)):
+            output = (output[:2] + ',' + output[2:truncDifference] +
+                      output[dotPos-1:inputSize])
+            '''inputsize +1 for comma.
+            length = max size of the string, excluding comma.
             else length = string size - 1'''
-        elif inputSize >= (length -2):
+            '''elif inputSize >= (length -1):'''
+        else:
+            truncDifference = length - inputSize
             '''if input = 1000, and is not 10,000 do'''
-            space = ' '
-            length = length +1
-            output = space + output[:1] + ',' + output[1:inputSize+2]
+            while spaceCount < truncDifference:
+                spaces = spaces + ' '
+                spaceCount +=1
+            if inputSize > (length - 2):
+                output = spaces + output[:1] + ',' + output[1:inputSize]
+            elif inputSize <= (length -2):
+                output = spaces + ' ' + output
     else:
-        if inputSize > (length):
-            truncDifference = (inputSize - length)
-            output = output[truncDifference:inputSize+1]
+        if (dotPos) > (length-2):
+            output = output[:truncDifference] + output[dotPos-1:inputSize]
         else:
             truncDifference = length - inputSize
             while spaceCount < truncDifference:
                 spaces = spaces + ' '
                 spaceCount +=1
-            output = spaces + output[:inputSize+1]            
+            output = spaces + output[:inputSize]
     return output
 
+
+def _test(): #req'd for doctest
+    """
+    >>> setOutput("String-Line")
+    just putting out String-Line
+
+    >>> setOutput(2)
+    Traceback (most recent call last):
+    ...
+    TypeError: cannot concatenate 'str' and 'int' objects
+    """
+    import doctest
+    doctest.testmod()
+
 if __name__ == "__main__":
-    TWOPLACES = Decimal(10) ** -2
-    outputFile = ""
-    Line2 = ("\nWC_LINE2\n          A.B. CREEDEE CO. LTD." +
-             "    PPAY PERIOD 01 PAGE ")
-    Line4 = ("WC_LINE4\nNUMBER NAME                 GROSS   TAX" +
-             "     UNION   MORT    NETT\n\n")
-    Line15 = ("\nWD_LINE15\nNUMBER OF EMPLOYEES      ")
-    Line17 = "WD_LINE17\n                                 TOTAL TAX   "
-    Line19 = "WD_LINE19\n                                 TOTAL NETT  "
-    pageCount = 0
-    totals = {'gross' : 0, 'tax' : 0, 'nett' : 0, 'union' : 0, 'mortgage' :
-              0, 'employees' : 0, 'exceptions' : 0}
-    pay9 = onRun()
+    dir, TWOPLACES, outputFile, Line2, Line4, Line15 = "", "", "", "", "", ""
+    Line17, Line19, pageCount, lineCount, employeeCount = "", "", "", "", ""
+    totals = {}
+    '''un-comment the below line to try doctests.
+    N.B. Doctest has been throwing weird concatenate errors.'''
+    #_test()
+    pay9 = initialise()
